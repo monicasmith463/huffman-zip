070793c2a613410a1e0d12da3698f34c21024c49
Monica Smith <monica.smith463@gmail.com>
2024-07-23 12:12:09 -0700

    huffman_tree: add node and tree class

---
+++ huffman_tree.py
@@ -0,0 +1,10 @@
+class HuffmanNode:
+    def __init__(self, char=None, freq=0):
+        self.char = char        
+        self.freq = freq         
+        self.left = None          
+        self.right = None        
+
+class HuffmanTree:
+    def __init__(self):
+        pass
